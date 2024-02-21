3bdc8241f87a87b244e5873fef408309e4b249bc
felipecrivera <felipecrivera.0987@gmail.com>
2024-02-21 15:50:19 -0600

    123

--- src/components/features/Dashboard/Dashboard.jsx
+++ src/components/features/Dashboard/Dashboard.jsx
@@ -1,22 +1,25 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const location = useLocation();
   const { customer } = location.state;
   const navigate = useNavigate();
-  if (customer.dashboardDisplay === "Both") {
-    return navigate(`/bookings/${customer._id}`, {
-      state: { customer: customer },
-    });
-  } else if (customer.dashboardDisplay === "Booking") {
-    return navigate(`/bookings/${customer._id}`, {
-      state: { customer: customer },
-    });
-  } else {
-    return navigate(`/activations/${customer._id}`, {
-      state: { customer: customer },
-    });
-  }
+  useEffect(() => {
+    if (customer.dashboardDisplay === "Both") {
+      return navigate(`/bookings/${customer._id}`, {
+        state: { customer: customer },
+      });
+    } else if (customer.dashboardDisplay === "Booking") {
+      return navigate(`/bookings/${customer._id}`, {
+        state: { customer: customer },
+      });
+    } else {
+      return navigate(`/activations/${customer._id}`, {
+        state: { customer: customer },
+      });
+    }
+  }, [])
 }
 
 export default Dashboard;
